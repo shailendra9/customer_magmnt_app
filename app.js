bbc74e477d156515a6444694149f6e2f0bf8d18d
shailendra pathak <shailendra9@users.noreply.github.com>
2015-07-20 10:22:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,12 +27,7 @@ define(['customersApp/services/routeResolver'], function () {
             var route = routeResolverProvider.route;
 
             $routeProvider
-                //route.resolve() now accepts the convention to use (name of controller & view) as well as the 
-                //path where the controller or view lives in the controllers or views folder if it's in a sub folder. 
-                //For example, the controllers for customers live in controllers/customers and the views are in views/customers.
-                //The controllers for orders live in controllers/orders and the views are in views/orders
-                //The second parameter allows for putting related controllers/views into subfolders to better organize large projects
-                //Thanks to Ton Yeung for the idea and contribution
+                
                 .when('/customers', route.resolve('Customers', 'customers/', 'vm'))
                 .when('/customerorders/:customerId', route.resolve('CustomerOrders', 'customers/', 'vm'))
                 .when('/customeredit/:customerId', route.resolve('CustomerEdit', 'customers/', 'vm', true))
